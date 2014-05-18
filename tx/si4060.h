0ad379356bc46cefa6a65453fda74ab97e8842be
Stefan Biereigel <stefan@biereigel.de>
2014-05-18 15:10:12 +0200

    add needed commands/properties/value definitions

--- tx/si4060.h
+++ tx/si4060.h
@@ -5,7 +5,7 @@
  *
  */
 
-/* command definitions */
+/* ===== command definitions ===== */
 #define CMD_POWER_UP		0x02
 #define CMD_NOP			0x00
 #define CMD_PART_INFO		0x01
@@ -16,7 +16,7 @@
 #define CMD_READ_CMD_BUF	0x44
 #define CMD_START_TX		0x31
 
-/* property group definitions */
+/* ===== property group definitions ===== */
 #define PROP_GLOBAL		0x00
 #define PROP_INT_CTL		0x01
 #define PROP_FRR_CTL		0x02
@@ -28,27 +28,20 @@
 #define PROP_SYNTH		0x23
 #define PROP_FREQ_CONTROL	0x40
 
-/* property definitions */
+/* ===== property definitions ===== */
+/* global properties */
 #define GLOBAL_XO_TUNE		0x00
-#define GLOBAL_CLK_CFG		0x01
 #define GLOBAL_CONFIG		0x03
-#define PREAMBLE_TX_LENGTH	0x00
-#define PREAMBLE_CONFIG_STD_1	0x01
-#define PREAMBLE_CONFIG_NSTD	0x02
-#define PREAMBLE_CONFIG		0x04
+/* sync properties */
 #define SYNC_CONFIG		0x11
+/* modem properties */
 #define MODEM_MOD_TYPE		0x00
-#define MODEM_MAP_CONTROL	0x01
-#define MODEM_DSM_CTRL		0x02
 #define MODEM_FREQ_DEV		0x0a
 #define MODEM_FREQ_OFFSET	0x0d
 #define MODEM_CLKGEN_BAND	0x51
-#define PA_MODE			0x00
-#define PA_PWR_LVL		0x01
+/* PA properties */
 #define PA_BIAS_CLKDUTY		0x02
-#define PA_TC			0x03
-#define PA_RAMP_EX		0x04
-#define PA_RAMP_DOWN_DELAY	0x05
+/* synthesizer properties */
 #define SYNTH_PFDCP_CPFF	0x00
 #define SYNTH_PFDCP_CPINT	0x01
 #define SYNTH_VCO_KV		0x02
@@ -57,8 +50,105 @@
 #define SYNTH_LPFILT1		0x05
 #define SYNTH_LPFILT0		0x06
 #define SYNTH_VCO_KVCAL		0x07
+/* frequency control properties */
 #define FREQ_CONTROL_INTE	0x00
 #define FREQ_CONTROL_FRAQ	0x01
 #define FREQ_CONTROL_CHANNEL_STEP_SIZE	0x04
 #define FREQ_CONTROL_W_SIZE	0x06
+
+/* ===== command arguments ===== */
+/* POWER_UP arguments */
+/* byte 1 */
+#define PATCH 		0x80 	/* set patch mode */
+#define FUNC		0x01 	/* power on device */
+/* byte 2 */	
+#define TXCO		0x01	/* select if TXCO (1) or crystal (0) is used */
+
+/* GPIO_PIN_CFG arguments */
+/* bytes 1 .. 6 */
+#define PULL_CTL 	0x40 	/* enable or disable pull-up resistor */
+/* bytes 1 .. 4 */
+#define GPIO_MODE_DONOTHING	0x00	/* pin behaviour is not changed */
+#define GPIO_MODE_TRISTATE	0x01	/* input and output drivers are disabled */
+#define GPIO_MODE_DRIVE0	0x02	/* CMOS output "low" */
+#define GPIO_MODE_DRIVE1	0x03	/* CMOS output "high" */
+#define GPIO_MODE_INPUT		0x04	/* GPIO is input, for TXDATA etc, function is not configured here */
+#define GPIO_MODE_32K_CLK	0x05	/* outputs the 32kHz CLK when selected in CLK32_CLK_SEL */		
+#define GPIO_MODE_BOOT_CLK	0x06	/* outputs boot clock when SPI_ACTIVE */
+#define GPIO_MODE_DIV_CLK	0x07	/* outputs divided xtal clk */
+#define GPIO_MODE_CTS		0x08	/* output, '1' when device is ready to accept new command */
+#define GPIO_MODE_INV_CNT	0x09	/* output, inverted CTS */
+#define GPIO_MODE_CMD_OVERLAP	0x0a	/* output, '1' if a command was issued while not ready */
+#define GPIO_MODE_SDO		0x0b	/* output, serial data out for SPI */
+#define GPIO_MODE_POR		0x0c	/* output, '0' while in POR state */
+#define GPIO_MODE_CAL_WUT	0x0d	/* output, '1' on expiration of wake up timer */
+#define GPIO_MODE_WUT		0x0e	/* wake up timer output */
+#define GPIO_MODE_EN_PA		0x0f	/* output, '1' when PA is enabled */
+#define GPIO_MODE_TX_DATA_CLK	0x10	/* data clock output, for TX direct sync mode */
+#define GPIO_MODE_TX_DATA	0x11	/* data output from TX FIFO, for debugging purposes */
+#define GPIO_MODE_IN_SLEEP	0x12	/* output, '0' when in sleep state */
+#define GPIO_MODE_TX_STATE	0x13	/* output, '1' when in TX state */
+#define GPIO_MODE_TX_FIFO_EMPTY	0x14	/* output, '1' when FIFO is empty */
+#define GPIO_MODE_LOW_BATT	0x15	/* output, '1' if low battery is detected */
+/* byte 5 omitted - no IRQ support */
+#define NIRQ_MODE_DONOTHING	0x00
+/* byte 6 omitted - no SDO reconfiguration support */
+#define SDO_MODE_DONOTHING	0x00
+/* byte 7 */
+#define DRV_STRENGTH_HIGH	(0x00 << 5)
+#define DRV_STRENGTH_MED_HIGH	(0x01 << 5)
+#define DRV_STRENGTH_MED_LOW	(0x02 << 5)
+#define DRV_STRENGTH_LOW	(0x03 << 5)
+
+/* START_TX arguments */
+/* byte 2 */
+#define START_TX_TXC_STATE_NOCHANGE	(0x00 << 4)
+#define START_TX_TXC_STATE_SLEEP	(0x01 << 4)
+#define START_TX_TXC_STATE_SPI_ACTIVE	(0x02 << 4)
+#define START_TX_TXC_STATE_READY	(0x03 << 4)
+#define START_TX_RETRANSMIT_0	(0x00 << 2)	/* send data that has been written to the TX FIFO */
+#define START_TX_START_IMM	(0x00 << 0)	/* start transmission immediately */
+
+/* ===== property values ===== */
+/* GLOBAL_CONFIG values */
+#define POWER_MODE_LOW_POWER	0x00	/* default */
+#define POWER_MODE_HIGH_PERF	0x01
+#define SEQUENCER_MODE_FAST	0x00	/* default */
+#define SEQUENCER_MODE_GUARANT	0x01
+/* SYNC_CONFIG values */
+#define SYNC_XMIT		(0x00 << 7)	/* default */
+#define SYNC_NO_XMIT		(0x01 << 7)
+/* MODEM_MOD_TYPE values */
+#define MOD_TYPE_CW		0x00
+#define MOD_TYPE_OOK		0x01
+#define MOD_TYPE_2FSK		0x02	/* default */
+#define MOD_TYPE_2GFSK		0x03
+#define MOD_TYPE_4FSK		0x04
+#define MOD_TYPE_4GFSK		0x05
+#define MOD_SOURCE_PACKET	(0x00 << 3) /* default */
+#define MOD_SOURCE_DIRECT	(0x01 << 3)
+#define MOD_SOURCE_PSEUDO	(0x02 << 3)
+#define MOD_GPIO_0		(0x00 << 5) /* default */
+#define MOD_GPIO_1		(0x01 << 5)
+#define MOD_GPIO_2		(0x02 << 5)
+#define MOD_GPIO_3		(0x03 << 5)
+#define MOD_DIRECT_MODE_SYNC	(0x00 << 7) /* default */
+#define MOD_DIRECT_MODE_ASYNC	(0x01 << 7)
+/* MODEM_CLKGEN_BAND values */
+#define SY_SEL_0		(0x00 << 3) /* low power */
+#define SY_SEL_1		(0x01 << 3) /* default */
+#define FVCO_DIV_4		0x00 /* default */
+#define FVCO_DIV_6		0x01
+#define FVCO_DIV_8		0x02 /* for 70cm ISM band */
+#define FVCO_DIV_12		0x03
+#define FVCO_DIV_16		0x04
+#define FVCO_DIV_24		0x05
+#define FVCO_DIV_24_2		0x06
+#define FVCO_DIV_24_3		0x07
+#define PA_BIAS_CLKDUTY_SIN_25	(0x03 << 6) /* for si4060  */
+
+
+
+
+
 
