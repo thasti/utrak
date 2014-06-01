ee336c83e4d70f51846528fb891326d609627861
Stefan Biereigel <stefan@biereigel.de>
2014-06-01 10:18:20 +0200

    add func prototype for i32toa

--- string.h
+++ string.h
@@ -4,6 +4,7 @@
 extern void atoi32(char *string, uint8_t len, uint32_t *integer);
 extern void atoid32(char *string, uint8_t len, uint32_t *integer, uint32_t *decimal);
 extern void atod32(char *string, uint8_t len, uint32_t *decimal);
+extern void i32toa(uint32_t in, uint8_t len, char *out);
 extern void i16tox(uint16_t x, char *out);
 
 #endif
