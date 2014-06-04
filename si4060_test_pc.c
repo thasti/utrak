1fc42fe81b23225d6da40a0f3465fafa16565de4
Stefan Biereigel <stefan@biereigel.de>
2014-06-04 09:53:42 +0200

    move si4060 power up before setup

--- si4060_test_pc.c
+++ si4060_test_pc.c
@@ -9,10 +9,10 @@
 #include <stdio.h>
 
 int main(void) {
-	printf("Si4060 Setup\n");
-	si4060_setup();
 	printf("\nSi4060 Power Up\n");
 	si4060_power_up();
+	printf("\nSi4060 Setup\n");
+	si4060_setup();
 	printf("\nSi4060 Nop\n");
 	si4060_nop();
 	printf("\nSi4060 Start TX\n");
