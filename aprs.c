9b79f9aa86ca340dc3616b8b0e874ba76e5c0ef5
Stefan Biereigel <stefan@biereigel.de>
2014-10-23 22:29:02 +0200

    implement APRS logic

--- aprs.c
+++ aprs.c
@@ -7,28 +7,179 @@
 #include "sin_table.h"
 
 extern volatile uint16_t aprs_tick;
+extern uint16_t aprs_buf_len;
+extern char aprs_buf[APRS_BUF_LEN];
+
+const char aprs_header[APRS_HEADER_LEN] = {
+	'A'*2, 'P'*2, 'R'*2, 'S'*2, ' '*2, ' '*2, SSID_RESC + DST_SSID,
+	'D'*2, 'K'*2, '3'*2, 'S'*2, 'B'*2, ' '*2, SSID_RESC + SRC_SSID,
+	'W'*2, 'I'*2, 'D'*2, 'E'*2, '1'*2, ' '*2, SSID_RESC + WIDE_SSID,
+	CONTROL_UI, PID_NONE};
+
+enum aprs_states {SM_INIT, SFLAG, AX25_HEADER, AX25_DATA, AX25_FCS1, AX25_FCS2, EFLAG};
+volatile uint16_t aprs_state = SM_INIT;
+volatile uint16_t fcs = 0;
+volatile uint8_t bitcnt = 8;
+volatile uint8_t onecnt = 0;
+volatile uint8_t finished = 0;
+
+
+inline void calculate_fcs(void) {
+	int i;
+	CRCINIRES = 0xffff;
+	for (i = 0; i < APRS_HEADER_LEN; i++) {
+		CRCDIRB_L = aprs_header[i];
+	}
+
+	for (i = 0; i < APRS_BUF_LEN; i++) {
+		CRCDIRB_L = aprs_buf[i];
+	}
+	fcs = CRCINIRES;
+}
+
+inline void aprs_init(void) {
+	aprs_state = SM_INIT;
+	finished = 0;
+	bitcnt = 8;
+	onecnt = 0;
+	/* copy LAT + dir */
+	/* copy LON + dir */
+	/* copy ALT_FT */
+	calculate_fcs();
+}
+
+/*
+ * get_next_byte
+ *
+ * fetches the next byte to be transmitted, until there is none left. it advances the
+ * internal AX.25 state machine. first, a configurable number of flags are returned,
+ * which are followed by the header, the payload, and end flags. an infinite number of
+ * end flags are returned, until the state machine is reset by init_aprs()
+ *
+ * returns:
+ * 	byte to be transmitted
+ */
+uint8_t get_next_byte(void) {
+	static int i = 0;
+	uint8_t retval;
+	switch (aprs_state) {
+		case SM_INIT:
+			aprs_state = SFLAG;
+			i = 0;
+		case SFLAG:
+			retval = AX25_FLAG;
+			if (++i >= AX25_SFLAGS) {
+				aprs_state = AX25_HEADER;
+				i = 0;
+			}
+			break;
+		case AX25_HEADER:
+			retval = aprs_header[i];
+			if (++i >= APRS_HEADER_LEN) {
+				aprs_state = AX25_DATA;
+				i = 0;
+			}
+			break;
+		case AX25_DATA:
+			retval = aprs_buf[i];
+			if (++i >= APRS_BUF_LEN) {
+				aprs_state = AX25_FCS1;
+				i = 0;
+			}
+			break;
+		case AX25_FCS1:
+			retval = (uint8_t)(fcs >> 8);
+			aprs_state = AX25_FCS2;
+			break;
+		case AX25_FCS2:
+			aprs_state = EFLAG;
+			retval = (uint8_t)fcs;
+			break;
+		case EFLAG:
+			retval = AX25_FLAG;
+			if (++i > AX25_EFLAGS) {
+				finished = 1;
+				i = 0;
+			}
+			break;
+		default:
+			retval = AX25_FLAG;
+	}
+	return retval;
+}
+
+/*
+ * get_next_bit
+ *
+ * fetches the next bit for the data stream.
+ *
+ * returns: the next bit to be transmitted, already NRZI coded and bit stuffed
+ */
+uint8_t get_next_bit(void) {
+	static uint8_t byte;
+	static uint8_t bit;
+	static uint8_t bit_d;
+	if (bitcnt >= 8) {
+		byte = get_next_byte();
+		bitcnt = 0;
+	}
+	/* all bytes LSB first */
+	bit = byte & 0x01;
+	if (bit) {
+		/* bit stuffing */
+		onecnt++;
+		if (aprs_state != SFLAG && aprs_state != EFLAG && (onecnt >= 5)) {
+			/* next time the same bit will be a zero -> stuffed bit */
+			byte &= ~BIT0;
+			onecnt = 0;
+		} else {
+			byte >>= 1;
+			bitcnt = (bitcnt + 1) & 0x07;
+		}
+
+	} else {
+		/* grab next bit only if not stuffed */
+		onecnt = 0;
+		byte >>= 1;
+		bitcnt = bitcnt + 1;
+	}
+	if (bit_d == bit) {
+		/* no transition */
+		/* save current bit */
+		bit_d = bit;
+		return 1;
+	} else {
+		/* transition */
+		/* save current bit */
+		bit_d = bit;
+		return 0;
+	}
+}
 
 void tx_aprs(void) {
-	serial_disable();
-	/* use 2FSK mode so we can adjust the OFFSET register */
-	si4060_setup(MOD_TYPE_2FSK);
-	si4060_start_tx(0);
 	uint16_t fcw = SPACE_FCW;
 	uint16_t pac = 0;
 	uint16_t offset = 0;
 	uint8_t samp_cnt = 0;
 
+	aprs_init();
+	serial_disable();
+
+	/* use 2FSK mode so we can adjust the OFFSET register */
+	si4060_setup(MOD_TYPE_2FSK);
+	si4060_start_tx(0);
+
 	aprs_tick = 0;
-	while(1) {
-		WDTCTL = WDTPW + WDTCNTCL + WDTIS1;
+	while(!finished) {
 		if (aprs_tick) {
 			/* running with APRS sample clock */
 			aprs_tick = 0;
 			samp_cnt++;
 			if (samp_cnt >= SAMP_PER_BIT) {
 				/* running with bit clock (1200 / sec) */
+				WDTCTL = WDTPW + WDTCNTCL + WDTIS1;
 				samp_cnt = 0;
-				if (fcw == MARK_FCW) {
+				if (get_next_bit()) {
 					fcw = SPACE_FCW;
 				} else {
 					fcw = MARK_FCW;
@@ -67,8 +218,8 @@ void tx_aprs(void) {
 			}
 		}
 	}
-	si4060_stop_tx();
 	si4060_set_offset(0);
+	si4060_stop_tx();
 	serial_enable();
 
 
--- aprs.h
+++ aprs.h
@@ -3,9 +3,41 @@
 
 void tx_aprs(void);
 
+/* APRS destination SSID is 0 */
+#define DST_SSID	0
+/* APRS source SSID */
+#define SRC_SSID	11
+/* WIDE1-x SSID */
+#define WIDE_SSID	3
+
 /* data from matlab script */
 #define MARK_FCW	146
 #define SPACE_FCW	268
 #define SAMP_PER_BIT	7
+
+/* AX.25 header consists of:
+ * 	7 bytes source
+ * 	7 bytes destination
+ * 	7 bytes path
+ * 	7 bytes repeater subfield
+ * 	1 byte control field
+ * 	1 byte PID field
+ */
+#define APRS_HEADER_LEN	23
+
+#define PID_NONE	0xf0
+#define CONTROL_UI	0x03
+#define SSID_RESC	0x60
+
+#define APRS_COMMENT	"uTrak " PAYLOAD_NAME
+#define APRS_BUF_LEN	1 + 8 + 1 + 9 + 4 + 6 + 1 + sizeof(APRS_COMMENT) - 1
+#define APRS_LAT_START	1
+#define APRS_LON_START	10
+#define APRS_ALT_START	22
+
+#define AX25_SFLAGS	50
+#define AX25_EFLAGS	10
+
+#define AX25_FLAG	0b01111110
 
 #endif
