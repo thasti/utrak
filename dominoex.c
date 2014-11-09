b6a9772f03d38bb43e09aed44adb3f18bef74b33
Stefan Biereigel <stefan@biereigel.de>
2014-11-09 09:23:28 +0100

    add dominoEX varicode table

---
+++ dominoex.c
@@ -0,0 +1,11 @@
+#include <inttypes.h>
+#include "dominoex-var.h"
+#include "si4060.h"
+#include "main.h"
+
+extern uint8_t varicode[128][3];
+
+void tx_dominoex(void) {
+
+}
+
---
+++ dominoex.h
@@ -0,0 +1,6 @@
+#ifndef __dominoex_h__
+#define __dominoex_h__
+
+void tx_dominoex(void);
+
+#endif
