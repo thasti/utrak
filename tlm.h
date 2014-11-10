bdc449d03b92a652506d1422f4eb8c3e92e41478
Stefan Biereigel <stefan@biereigel.de>
2014-11-10 13:28:21 +0100

    adapt blip ratio to TLM mode used
    because of different timer speeds, in DominoEX mode, less interrupts
    need to be waited for output state change of OOK signal

--- tlm.h
+++ tlm.h
@@ -2,6 +2,16 @@
 #define __tlm_h__
 
 #include <inttypes.h>
+#include "main.h"
+
+/* because tick is slower when using DominoEX,
+ * use less ticks for blipping
+ */
+#ifdef TLM_DOMINOEX
+#define BLIP_FACTOR	1
+#else
+#define	BLIP_FACTOR	5
+#endif
 
 void tx_blips(uint8_t sats);
 void init_tx_buffer(void);
