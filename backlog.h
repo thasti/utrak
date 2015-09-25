150a474fb4202a0b6d5b15430226cf6356afb521
Stefan Biereigel <stefan@biereigel.de>
2015-09-25 15:43:47 +0200

    changes for 0x0b

--- backlog.h
+++ backlog.h
@@ -4,7 +4,7 @@
 #include "fix.h"
 
 /* number of backlog fixes to save*/
-#define BACKLOG_MAX_ENTRIES	72	
+#define BACKLOG_MAX_ENTRIES	24
 /* number of backlog fixes to skip between transmissions.
  * this distributes transmitted backlog fixes more evenly, so that after long
  * out-of-range-times the history is filled in an "interleaved" fashion
@@ -12,7 +12,7 @@
  * be sure to set this value so that different fixes get transmitted every time!
  * if the number of fixes is even, this number must be uneven!
  */
-#define BACKLOG_TX_INCREMENT	5
+#define BACKLOG_TX_INCREMENT	1
 
 void backlog_add_fix(struct gps_fix *fix);
 struct gps_fix* backlog_get_next_fix(void);
